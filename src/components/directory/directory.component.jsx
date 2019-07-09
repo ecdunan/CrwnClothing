0890ac651c69fc64497b95a49cc70de581057d4b
ecdunan <dunan.emmanuel@gmail.com>
2019-07-10 06:32:14 +0800

    Separated to initial components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { sections } from '../../data/directory.data.js';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections
+        };
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({title, imageUrl, id}) =>
+                        <MenuItem key={id} title={title}/>
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+export default Directory;
