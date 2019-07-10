f4c7e06726a94eeb3cd858755ee0901a851d9f08
ecdunan <dunan.emmanuel@gmail.com>
2019-07-11 06:42:46 +0800

    Added collections styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import Shop from './pages/shop/shop.component';
 import './App.css';
-import HomePage from './pages/homepage.component';
-
-const HatsPage = () => (
-    <div>
-        <h1> HATS PAGE </h1>
-    </div>
-);
 
 function App() {
   return (
     <div>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={Shop} />
+        </Switch>
     </div>
   );
 }
