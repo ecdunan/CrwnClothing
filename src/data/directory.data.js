228a9034b9adbdddd955b2b6f8e607eb0ec10283
ecdunan <dunan.emmanuel@gmail.com>
2019-07-11 04:36:56 +0800

    Added basic routing

--- src/data/directory.data.js
+++ src/data/directory.data.js
@@ -4,34 +4,34 @@ export const sections = [
     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     id: 1,
     size: 'medium',
-    linkUrl: 'shop/hats'
+    linkUrl: 'hats'
   },
   {
     title: 'jackets',
     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     id: 2,
     size: 'medium',
-    linkUrl: 'shop/jackets'
+    linkUrl: 'jackets'
   },
   {
     title: 'sneakers',
     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     id: 3,
     size: 'medium',
-    linkUrl: 'shop/sneakers'
+    linkUrl: 'sneakers'
   },
   {
     title: 'womens',
     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     size: 'large',
     id: 4,
-    linkUrl: 'shop/womens'
+    linkUrl: 'womens'
   },
   {
     title: 'mens',
     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     size: 'large',
     id: 5,
-    linkUrl: 'shop/mens'
+    linkUrl: 'mens'
   }
 ];
