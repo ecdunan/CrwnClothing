9e339e8ea13159615c28b20cb55d8d492ae1835c
ecdunan <dunan.emmanuel@gmail.com>
2019-07-14 02:13:38 +0800

    Added sign in component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 
@@ -14,6 +15,7 @@ function App() {
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={Shop} />
+            <Route path='/signin' component={SignInAndSignUp} />
         </Switch>
     </div>
   );
