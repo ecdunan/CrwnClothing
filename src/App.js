c9de16b93e8c7fe62e3956a19e0cb9a3ce4e9c8c
ecdunan <dunan.emmanuel@gmail.com>
2019-07-14 00:45:25 +0800

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
+
 import './App.css';
 
 function App() {
   return (
     <div>
+        <Header />
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={Shop} />
