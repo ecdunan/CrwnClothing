e7cd6ed119e0fcf59177871b06859b32d05a7fc1
ecdunan <dunan.emmanuel@gmail.com>
2019-07-22 03:24:56 +0800

    Added redirect if login user accessing signinsignup up page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -9,6 +9,10 @@ import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.com
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 import './App.css';
+
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser
+});
 
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
@@ -47,11 +51,15 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage} />
                     <Route path='/shop' component={Shop} />
-                    <Route path='/signin' component={SignInAndSignUp} />
+                    <Route exact path='/signin'
+                        render={() => this.props.currentUser ?
+                            <Redirect to='/' />
+                            : <SignInAndSignUp />
+                        } />
                 </Switch>
             </div>
         );
     }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
